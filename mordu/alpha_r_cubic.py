b781f273a7f3f3387c53aabfb454555b44c72871
cat <catarinaduartenunes@gmail.com>
2026-01-20 14:08:22 +0000

    docstrings for the alpha_r_cubic

--- mordu/alpha_r_cubic.py
+++ mordu/alpha_r_cubic.py
@@ -3,30 +3,35 @@
 #sympy symbols
 from .symbols import *
 
+#pure fluid object
+from purefluid import PureFluid
+
 # cubic non dimensional residual Helmholts energy class
 # necessary to create any cubic EOS for a pure fluid
 class AlphaRCubic():
-    """
-    Class for calculating the residual Helmholtz energy of cubic equations of state.
+    """The non-dimensional residual Helmholtz energy for a cubic equation of state
 
-    This class provides methods to compute the residual Helmholtz energy as a function of temperature and density.
-    The expression for the Helmholtz energy is created by substituting the symbols for values included in the input dictionary.
-
-    Attributes:
-        expression (sp.core.add.Add): The expression for the residual Helmholtz energy.
-        a (float): The parameter 'a' related to the equation of state.
-        b (float): The parameter 'b' related to the equation of state.
-        alpha_r (sp.core.add.Add): The evaluated residual Helmholtz energy after substituting 'a' and 'b' into the expression.
-
-    Methods:
-        for_purefluid(cls, fluid, alpha_r_expr, a_c_expr, alpha_T_expr, b_expr):
-            Class method to create an instance for a pure fluid using the provided expressions and fluid properties.
-
-        for_mixture(cls, mix, mixture_rule, cubic_alpha_r_list, alpha_r_expr, **kwargs):
-            Class method to create an instance for a mixture using the provided mixture rule and expressions.
+    Each equation of state uses a different definition for the residual Helmholtz energy.
+    This definition works for cubic equations of state only.
+    This definition **does not** include an option for volume translation.
     """
 
-    def __init__(self, alpha_r_expr: sp.core.add.Add, a_value,  b_value):
+    def __init__(self, alpha_r_expr: sp.Expr, a_value: float,  b_value: float):
+        """Create the non-dimensional Helmholtz energy for a cubic equation of state
+
+        Parameters
+        ----------
+        alpha_r_expr: sp.Expr
+            Sympy expression for the non-dimensional helmholtz energy as a function 
+            of the symbols T (temperature in [K]), rho (density in [mol/m3]),
+            a (a parameter in []), b (b parameter in [])
+        a_value: sp.Expr
+            Sympy expression of the a parameter for the specific fluid for the specific EOS
+            This value can be a dependent of the symbol T (temperature in [K])
+        b_value: float
+            Value of the b parameter for the specific fluid for the specific EOS
+            This value is almost always a float
+        """
         self.expression = alpha_r_expr
 
         self.a = a_value
@@ -39,7 +44,33 @@ class AlphaRCubic():
 
     #for a pure fluid
     @classmethod
-    def for_purefluid(cls, fluid, alpha_r_expr: sp.core.add.Add, a_c_expr: sp.core.mul.Mul, alpha_T_expr: sp.core.power.Pow, b_expr: sp.core.mul.Mul):
+    def for_purefluid(cls, fluid: PureFluid, alpha_r_expr: sp.Expr, a_c_expr: sp.Expr, alpha_T_expr: sp.Expr, b_expr: sp.Expr):
+        """Create the non-dimensional Helmholtz energy of a cubic equation of state for a specific pure fluid
+        
+        Parameters
+        ----------
+        fluid: PureFluid
+            A fluid object from the PureFluid class.
+        alpha_r_expr: sp.Expr
+            A sympy expression for the non-dimensional helmholtz energy as a function 
+            of the symbols T (temperature in [K]), rho (density in [mol/m3]),
+            a (a parameter in []), b (b parameter in []).
+        a_c_expr: sp.Expr
+            A sympy expression for the a_c coefficient of the specific cubic EOS.
+            This expression is most often dependent on the T_c (the critical temperature
+            of the fluid in [K]) and P_c (the critical pressure of the fluid in [Pa]) 
+            sympy symbols.
+        alpha_T_expr: sp.Expr
+            A sympy expression for the alpha_T coefficient of the specific cubic EOS.
+            This expression can be a function of T (temperature in [K]), T_c (the critical
+            temperature of the fluid in [K]) and omega (the accentric factor of the fluid).
+            In the specific case of the van-der-Waals EOS, alpha_T is a simpified 1.
+        b_expr: sp.Expr
+            A sympy expression for the b coefficient of the specific cubic EOS.
+            This expression is most often dependent on the T_c (the critical temperature
+            of the fluid in [K]) and P_c (the critical pressure of the fluid in [Pa]) 
+            sympy symbols.
+        """
         a_c_value = a_c_expr.subs([ (T_c, fluid.T_c), (P_c, fluid.P_c)])
         alpha_T_value = alpha_T_expr.subs([(omega, fluid.omega), (T_c, fluid.T_c)])
 
